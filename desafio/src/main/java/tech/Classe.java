8b49aff61dff0e0259a063f50361adf679c4a775
Gisele <giseledasilvac@gmail.com>
2024-02-19 20:12:07 -0300

    commit inicial Pessoa

---
+++ desafio/src/main/java/tech/Classe.java
@@ -0,0 +1,15 @@
+package tech;
+
+public class Classe {
+
+    public static void main (String[] args){
+        Pessoa pessoa1 = new Pessoa();
+
+        pessoa1.nome = "Joao";
+
+        pessoa1.metodox();
+
+    }
+
+
+}
